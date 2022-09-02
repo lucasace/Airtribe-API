c769a8a0d8f2862292716602c0c2e190eb86811f
lucasace <tauro.royston@gmail.com>
2022-09-03 02:31:33 +0530

    [FEATURE] Initial db design

---
+++ migrations/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO INSTRUCTORS(ID, NAME, EMAIL, DESIGNATION, COMPANY, LINKEDIN, ABOUT) VALUES(
+    DEFAULT, 'John Doe', 'xyz@gmail.com', 'Developer', 'ABC', 'www.linkedin.com/john-doe', 'I am a developer'
+);
+
+INSERT INTO INSTRUCTORS(ID, NAME, EMAIL, DESIGNATION, COMPANY, LINKEDIN, ABOUT) VALUES(
+    DEFAULT, 'Jane Doe', 'jane@gmail.com', 'Designer', 'DEF', 'www.linkedin.com/jane-doe', 'I am a designer'
+);
+
+INSERT INTO INSTRUCTORS(ID, NAME, EMAIL, DESIGNATION, COMPANY, LINKEDIN, ABOUT) VALUES(
+    DEFAULT, 'John Smith', 'smith@outlook.com', 'Developer', 'GHI', 'www.linkedin.com/john-smith', 'I am a developer'
+);
+
+INSERT INTO USERS(ID, NAME, EMAIL) VALUES(
+    DEFAULT, 'Johnny Doe', 'johnny@gmail.com'
+);
+
+INSERT INTO USERS(ID, NAME, EMAIL) VALUES(
+    DEFAULT, 'Janet smith', 'janet@gmail.com'
+);
+
+INSERT INTO USERS(ID, NAME, EMAIL) VALUES(
+    DEFAULT, 'Lucas Bing', 'lucas@gmail.com'
+);
